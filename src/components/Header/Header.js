05d51c93840b2f70f430965a5c4a6ed7e647e475
davidabutler92 <davidabutler92@yahoo.com>
2021-02-01 14:35:57 -0800

    add material-ui dependency

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import { HashLink as Link } from 'react-router-hash-link';
+import { NavHashLink as Link } from 'react-router-hash-link';
 
 import styles from './Header.css';
 
 export default function Header() {
   return (
     <div className={styles.root}>
-      <Link smooth to="/#section-home">
+      <Link
+        smooth
+        activeClassName="selected"
+        activeStyle={{ color: 'lightblue' }}
+        to="/#section-home"
+      >
         Home
       </Link>
-      <Link smooth to="/about#section-about">
+      <Link
+        smooth
+        activeClassName="selected"
+        activeStyle={{ color: 'lightblue' }}
+        to="/about#section-about"
+      >
         About
       </Link>
-      <Link smooth to="/portfolio#section-portfolio">
+      <Link
+        smooth
+        activeClassName="selected"
+        activeStyle={{ color: 'lightblue' }}
+        to="/portfolio#section-portfolio"
+      >
         Portfolio
       </Link>
-      <Link smooth to="/resume#section-resume">
+      <Link
+        smooth
+        activeClassName="selected"
+        activeStyle={{ color: 'lightblue' }}
+        to="/resume#section-resume"
+      >
         Resume
       </Link>
-      <Link smooth to="/contact#section-contact">
+      <Link
+        smooth
+        activeClassName="selected"
+        activeStyle={{ color: 'lightblue' }}
+        to="/contact#section-contact"
+      >
         Contact
       </Link>
     </div>
